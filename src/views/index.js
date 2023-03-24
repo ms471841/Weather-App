4fa482a344f21b1d38516a1667845583f6d5f9d3
ms471841 <ms471841@gmail.com>
2023-03-24 23:34:52 +0530

    update

--- src/views/index.js
+++ src/views/index.js
@@ -28,65 +28,69 @@ function WeatherApp() {
   }, [dispatch]);
 
   return (
-    <Box sx={{ backgroundColor: "black", margin: 0 }}>
-      <Box
-        // ref={appRef}
-        sx={{
-          marginX: "30px",
-          backgroundImage: `url(${bgImage})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          minHeight: "120vh",
-        }}
-      >
-        <Box
-          sx={{
-            // maxWidth: "500px",
-            margin: "0 auto",
-          }}
-        >
-          <Typography variant="h4" align="center" gutterBottom>
-            Home Cities News
-          </Typography>
+    <>
+      {weatherData && (
+        <Box sx={{ backgroundColor: "black", margin: 0 }}>
           <Box
+            // ref={appRef}
             sx={{
-              display: "flex",
-              justifyContent: "flex-end",
-              height: 200,
+              marginX: "30px",
+              backgroundImage: `url(${bgImage})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              minHeight: "120vh",
             }}
           >
-            <Card sx={{ width: "400px", backgroundColor: "", p: "10px" }}>
-              <CardContent>
-                <Typography fontWeight={"bold"} variant="h4" gutterBottom>
-                  Weather Details
-                </Typography>
-                <Typography>Cloudy : {weatherData.clouds.all}</Typography>
-                <Typography>Humidity : {weatherData.main.humidity}%</Typography>
-                <Typography>Wind : {weatherData.wind.speed}KM/H</Typography>
-              </CardContent>
-            </Card>
-          </Box>
+            <Box
+              sx={{
+                // maxWidth: "500px",
+                margin: "0 auto",
+              }}
+            >
+              <Typography variant="h4" align="center" gutterBottom>
+                Home Cities News
+              </Typography>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  height: 200,
+                }}
+              >
+                <Card sx={{ width: "400px", backgroundColor: "", p: "10px" }}>
+                  <CardContent>
+                    <Typography fontWeight={"bold"} variant="h4" gutterBottom>
+                      Weather Details
+                    </Typography>
+                    <Typography>Cloudy : {weatherData.clouds.all}</Typography>
+                    <Typography>
+                      Humidity : {weatherData.main.humidity}%
+                    </Typography>
+                    <Typography>Wind : {weatherData.wind.speed}KM/H</Typography>
+                  </CardContent>
+                </Card>
+              </Box>
 
-          {weatherData && (
-            <Box sx={{ marginTop: "20rem", marginX: "100px" }}>
-              <Typography variant="h5" gutterBottom>
-                {weatherData.name}, {weatherData.sys.country}
-              </Typography>
-              <Typography variant="h6" gutterBottom>
-                {weatherData.weather[0].description}
-              </Typography>
-              <Typography variant="h1" gutterBottom>
-                {Math.round(weatherData.main.temp)}°C
-              </Typography>
+              <Box sx={{ marginTop: "20rem", marginX: "100px" }}>
+                <Typography variant="h5" gutterBottom>
+                  {weatherData.name}, {weatherData.sys.country}
+                </Typography>
+                <Typography variant="h6" gutterBottom>
+                  {weatherData.weather[0].description}
+                </Typography>
+                <Typography variant="h1" gutterBottom>
+                  {Math.round(weatherData.main.temp)}°C
+                </Typography>
+              </Box>
             </Box>
-          )}
+          </Box>
+          <WeatherCard />
+          <EarthCard />
+          <NewsCard />
+          <BottomCard />
         </Box>
-      </Box>
-      <WeatherCard />
-      <EarthCard />
-      <NewsCard />
-      <BottomCard />
-    </Box>
+      )}
+    </>
   );
 }
 
